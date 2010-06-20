f2ca20c31389db7bb83fbd3b8bc79e514e44e765
claus <claus_herther@yahoo.com>
2010-06-20 18:20:33 +0200

    Changed time input to +/- buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function setWorkoutLength(operation){
+	var step = 15;
+	var max = 4*8*step;
+	var length = $('#workout_workout_length');
+	var currentVal = parseInt(length.val());
+	var newVal = operation == '-' ? 
+		currentVal > step ? currentVal - step : currentVal
+	: currentVal < max ? currentVal + step : currentVal;
+	length.val(newVal);
+	
+	$('#cw-length-label > span').text(newVal);
+	var msg = '';
+	if (newVal >= 120) {
+			msg = 'Really!?';
+	} else if (newVal >= 90){
+			msg = 'Wow!';
+	} else if (newVal >= 60){
+			msg = 'Nice!';
+	}
+	$('#cw-length-msg').text(msg);
+}
